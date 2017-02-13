31283cc7a71acf90f66ec6d052edef45c9ca06be
sudipta1411 <sdptroy3@gmail.com>
2017-02-13 10:31:58 +0530

    Assignment1 done

--- main.c
+++ main.c
@@ -7,13 +7,13 @@
 int main(int argc, char* argv[])
 {
     byte_t* byte = byte_alloc(0);
-    uint8_t bytes[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    /*uint8_t bytes[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};*/
     //uint8_t bytes[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
     //uint8_t bytes[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     omp_set_num_threads(NUM_THREAD);
-    size_t nz = /*NUM;*/ARR_SIZE(bytes);
-    byte_array_t* b_ar = byte_array_create(bytes, nz);
+    size_t nz = NUM;//ARR_SIZE(bytes);
+    byte_array_t* b_ar = byte_array_create_copy(0,nz);//byte_array_create(bytes, nz);
     fprintf(stdout, "BYTE ARRAY SIZE : %zu\n", get_array_size(b_ar));
     uint8_t ret = do_or_tree(b_ar); //do_or_array(b_ar);
     fprintf(stdout, "OR RESULT : %u\n", ret);
